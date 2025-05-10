3c687b1cdade57895f7a4922b66c8107966657fb
Kavi-m2 <kavineshm7@gmail.com>
2025-05-10 10:46:31 +0530

    Point to Render backend

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     setError('');
     setResult(null);
     try {
-      const res = await fetch('http://127.0.0.1:5000/predict', {
+      const res = await fetch('https://solar-be.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
